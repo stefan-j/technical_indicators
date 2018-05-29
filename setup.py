c4adf362dc67416f6b9c7a61a92d9cae33c62193
Stefan Jacholke <stefanjacholke@gmail.com>
2018-05-29 09:27:13 +0200

    Remove py2exe

--- setup.py
+++ setup.py
@@ -9,10 +9,9 @@ from __future__ import (absolute_import, division, print_function,
 import sys
 from os import path
 from setuptools import setup, find_packages
-import py2exe
 from technical_indicators import (NAME, VERSION, DESC, LONG_DESC, LICENSE, URL,
-                                  AUTHOR, EMAIL, KEYWORDS, CLASSIFIERS, SCRIPT,
-                                  DATA_FILES, DATA_FILES_PY2EXE)
+                                  AUTHOR, EMAIL, KEYWORDS, CLASSIFIERS, 
+                                  DATA_FILES)
 
 
 #PACKAGES = [NAME] # only used if find_packages() does not work
@@ -32,21 +31,7 @@ if path.isfile(REQUIREMENTS_FILE):  # if file exists
         REQUIREMENTS = f.read()
         ##REQUIREMENTS = f.read().splitlines()
 
-# if not cleared they are added to bdist_egg root
-if sys.argv[1] and str.lower(sys.argv[1]) != 'py2exe':
-    DATA_FILES_PY2EXE = ''
-
-OPTIONS = {'py2exe': {'compressed': True,
-                      'ascii': False,
-                      #'packages': NAME,
-                      #'bundle_files': 1, # exe does not work
-                      #'includes': ['numpy'],
-                      #'excludes': ['doctest', 'pdb', 'unittest', 'difflib',
-                      #             'inspect', 'pyreadline', 'optparse',
-                      #             'calendar', 'email', '_ssl', 'pickle',
-                      #             # 'locale'
-                      #             ]
-                      }
+OPTIONS = {
            }
 
 setup(name=NAME,
@@ -85,9 +70,6 @@ setup(name=NAME,
       zip_safe=False,
 
       # py2exe config
-      console=[SCRIPT],
-      options=OPTIONS,
-      data_files=DATA_FILES_PY2EXE,
       #windows=[{'script': '__main__.py',
       #          'icon_resources': [(0, 'daysgroudned.ico')]
       #          }],
